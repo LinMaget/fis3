dfe2acaaf75ecd8bb7529fd754468f3c63bead06
linyuanteng <linyuanteng@masget.com>
2016-04-01 16:54:22 +0800

    up

--- fis-conf.js
+++ fis-conf.js
@@ -1,17 +1,19 @@
-//设置项目源码
-fis.set('project.ignore', [
-  'components/**',
-  '*',              //单个文件不发布
-]);
+//fis.set('project.ignore', [    //  match 匹配的内容也不会被发布
+//  'components/**',
+//  '*',              //单个文件不发布
+//]);
+
+
 //框架基本js css打包路径
 fis.set('frame','/static/base');
-
-
+fis.match("**",{  //默认没规则没匹配到文件不发布
+  release : false
+})
 //打包js --BG
-  fis.media('jq').match('/components/(jquery/*.js)', {    //jquery
+  fis.match('/components/(jquery/*.js)', {    //jquery
     release: '${frame}/$1'
   })
-  fis.match('/components/(angular/*.js)', {               //angular
+      .match('/components/(angular/*.js)', {               //angular
     release: '${frame}/$1'
   })
 //打包js --END
@@ -23,7 +25,7 @@ fis.set('frame','/static/base');
 //打包CSS --END
 
 //打包模板
-  fis.match('/indexTemp/(*.html)', {   //bootstrap  css
-    useCache: false,
+  fis.match('/indexTemp/(*.html)', {  //html特性  会引入页面css,js依赖
+    isJsLike: true,    //如果当做JS来处理 就不会自动导入页面上的 css js依赖
     release: 'index$3'
   })
